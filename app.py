a2de1c8214f7e64cdf4c1b1057a7f1e5a3674a9a
isfa18 <114433637+isfa18@users.noreply.github.com>
2026-04-28 15:09:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,52 @@
 from flask import Flask, request, jsonify
 import pandas as pd
-from sklearn.cluster import KMeans
+from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import os
 
 app = Flask(__name__)
 
-# =====================
-# LOAD DATA
-# =====================
-data = pd.read_csv("dataset_penjualan_rongsok_update_v2.csv", sep=";")
+base_dir = os.path.dirname(__file__)
+file_path = os.path.join(base_dir, "dataset_penjualan_rongsok_update_v2.csv")
+
+data = pd.read_csv(file_path, sep=";")
+
+# rapikan data
+data["barang"] = data["barang"].str.lower()
 
 model = {}
 
 for barang in data["barang"].unique():
-    subset = data[data["barang"] == barang]["berat_keluar_kg"].values.reshape(-1,1)
+    subset = data[data["barang"] == barang]
 
-    kmeans = KMeans(n_clusters=2, random_state=42)
-    kmeans.fit(subset)
+    # fitur (X) → stok & penjualan
+    X = subset[["stok", "berat_keluar_kg"]]
 
-    centers = kmeans.cluster_centers_.flatten()
-    cluster_jual = np.argmax(centers)
+    # label (y) → buat sendiri
+    y = np.where(subset["berat_keluar_kg"] > subset["berat_keluar_kg"].mean(), "JUAL", "TUNGGU")
 
-    model[barang] = {
-        "kmeans": kmeans,
-        "cluster_jual": cluster_jual
-    }
+    clf = DecisionTreeClassifier()
+    clf.fit(X, y)
+
+    model[barang] = clf
+
 
 def prediksi_jual(barang, stok):
+    barang = barang.lower()
+
     if barang not in model:
         return "DATA TIDAK ADA"
 
-    kmeans = model[barang]["kmeans"]
-    cluster_jual = model[barang]["cluster_jual"]
+    subset = data[data["barang"] == barang]
 
-    pred_cluster = kmeans.predict([[stok]])[0]
+    rata_keluar = subset["berat_keluar_kg"].mean()
 
-    return "JUAL" if pred_cluster == cluster_jual else "TUNGGU"
+    # prediksi pakai model
+    clf = model[barang]
+    pred = clf.predict([[stok, rata_keluar]])[0]
+
+    return pred
+
 
 @app.route('/prediksi', methods=['POST'])
 def prediksi():
@@ -52,7 +62,6 @@ def prediksi():
         "stok": stok,
         "rekomendasi": hasil
     })
-
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
